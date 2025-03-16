9e84b91f1b011d68de4f2d16be2c141813e3c336
AjaeroElvis4 <AjaeroElvis4@gmail.com>
2025-03-16 01:07:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "ajaeroelvis4@gmail.com";
 const pass = "ylxnobprypgjidpw";
-// 01 APRIL
+// 15 APRIL
 
 // Middleware
 app.use(express.json());

